adf84f9a71c6205ed52e462b2f54ab8a8e049eb3
Lucia Jimena Cartagena Miranda <148520491+luciajcm@users.noreply.github.com>
2025-09-24 07:43:03 -0500

    Add files via upload

--- ingesta.py
+++ ingesta.py
@@ -1,7 +1,7 @@
 import boto3
 
 ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "gcr-output20252-01"
 
 s3 = boto3.client('s3')
 response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
